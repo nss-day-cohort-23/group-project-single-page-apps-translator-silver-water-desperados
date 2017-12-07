dc373a0dc17d2eab07ee082668d9272e1e1e6ae5
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-07 13:33:31 -0600

    added event listener function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,18 @@
 "use strict";
 
+
+const french = require('./french.js');
+//console.log(french.translateToFrench("merry christmas")); 
+// const varName = require(‘./filename’);
+
+const german = require('./german.js');
+//console.log(german.translateToGerman("merry christmas"));
+
+const spanish = require('./spanish.js');
+//console.log(spanish.translateToSpanish("merry christmas"));
+
+
+
 let userInput = document.getElementById("input");
 let spanishBtn = document.getElementById("spanishBtn");
 let germanBtn = document.getElementById("germanBtn");
@@ -7,24 +20,11 @@ let frenchBtn = document.getElementById("frenchBtn");
 let translate = document.getElementById("translate");
 let userOutput = document.getElementById("output");
 
-spanishBtn.addEventListener("click", );
-germanBtn.addEventListener("click", );
-frenchBtn.addEventListener("click", );
 
-
-
-
-
-
-
-
-const french = require('./french.js');
-console.log(french.translateToFrench("merry christmas")); 
-// const varName = require(‘./filename’);
-
-const german = require('./german.js');
-console.log(german.translateToGerman("merry christmas"));
-
-const spanish = require('./spanish.js');
-console.log(spanish.translateToSpanish("merry christmas"));
+translate.addEventListener("click", function() {
+    if (germanBtn.checked) {
+      console.log("german button is clicked");
+      userOutput.innerHTML = german.translateToSpanish(userInput.value);
+    } 
+  }); 
 
